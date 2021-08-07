53098d270ce04a910a4350082b9c7d5ff33316c3
joshard <jeremj98@gmail.com>
2021-08-07 16:01:55 +0300

    api

---
+++ account/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from knox.models import AuthToken
+from .serializers import UserSerializer, RegisterSerializer
+
+from rest_framework import permissions, status
+from django.contrib.auth.models import User, Group, auth
+from django.contrib.auth import login
+
+from rest_framework import permissions
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.views import LoginView as KnoxLoginView
+from django.shortcuts import redirect, render
+from rest_framework.decorators import api_view, permission_classes, action
+
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)
+        # return redirect("home")
+
+# Register API
+
+
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)[1]
+        })
+
+
+@ api_view(['GET', 'POST'])
+@ permission_classes((permissions.AllowAny,))
+def home(request, user):
+
+    user = User.objects.get(username=user)
+
+    if user is None:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == "GET":
+        serializers = UserSerializer(user)
+        serializer_context = {
+            'request': request,
+        }
+        # return Response(serializers.data)
+        return render(request, 'home.html', context=serializer_context)
+
+# class Logout(APIView):
+#     def get(self, request, user):
+#         # simply delete the token to force a login
+#         user.auth_token.delete()
+#         return Response(status=status.HTTP_200_OK)
+#         return render(request, 'api/login.html')
